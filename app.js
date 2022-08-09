4435050695506c2114a9ce8ebb0e6d2f33216bc4
fabrizio <fabricimarelli@gmail.com>
2022-08-09 16:51:16 -0300

    guardado de partida en local storage

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 window.onload=function(){
 
-    var juego_comienza=0;//OK
+    var juego_comienza=0;
     var nombre_jugador = document.getElementById('nombre');
-    var jugador//OK
-    var play_end=false;//OK
-    var today_word//OK
-    var path='palabras.json'//OK
-    var fila=1;//OK
-    var letra=1;//OK
+    var jugador;
+    var play_end=false;
+    var play_load=false;
+    var today_word
+    var path='palabras.json'
+    var fila=1;
+    var letra=1;
     var palabra=document.querySelectorAll('.palabra_ingresada');//OK
     var palabra_encontrada=false;
     var span = document.getElementsByClassName("close")[0];
@@ -17,6 +18,8 @@ window.onload=function(){
     var parrafo_gana = document.getElementById("gana");
     var parrafo_pierde = document.getElementById("pierde");
     var modal_abierto=false;
+    var captura_temporizador;
+    var tablero_celdas=[];
 
      
 
@@ -28,7 +31,6 @@ window.onload=function(){
                 today_word = data[Math.floor(Math.random()*data.length)].toUpperCase()
                 today_word_div=[...today_word]//Array.from(today_word);
                 console.log('La palabra de hoy es: '+today_word);
-                console.log('la palabra dividida: '+today_word_div);
                 
             })
         })
@@ -43,12 +45,11 @@ window.onload=function(){
     //variable para ver que boton presionamos
     var boton=document.querySelectorAll('button');
 
-    //loop para ver que boton se presiono-----------------------------OK
+    //para ver que boton se presiona cada vez-----------------------------OK
     boton.forEach((elemento) => {
         elemento.addEventListener('click', function(){
             var tecla_presionada=elemento.attributes["data-key"].value;
             key_press(tecla_presionada)
-            console.log('presione la tecla: '+tecla_presionada+'. El juego esta terminado:'+play_end+'. El juego comenzo: '+juego_comienza);
 
         })
     })
@@ -57,14 +58,21 @@ window.onload=function(){
     function key_press(tecla){
         if(!play_end){
             if(tecla=="jugar"){
-                if(nombre_jugador.value.length==0){
-                    console.log('entre')
+                if(play_load && juego_comienza===0){
+                    juego_comienza=1;
+                    iniciar_temp();
+                    console.log("La palabra recuperada es: "+today_word)
+                } else if(nombre_jugador.value.length==0 && juego_comienza===0){
+                    console.log('entre al error de no poner nombre')
                     modal_nombre.classList.remove('modal_off');
                     modal_nombre.classList.add('modal_on');
                     
+                } else if(juego_comienza===1){
+                    alert("Ya hay un juego en curso");
                 } else{
                     juego_comienza=1;
                     iniciar_temp();
+                    console.log("El nombre del jugador es "+nombre_jugador.value);
                 }               
 
             } else if(tecla=="contacto"){
@@ -73,16 +81,18 @@ window.onload=function(){
                 enviar_palabra();
             } else if (tecla==="borrar" && juego_comienza==1){
                 eliminar_letra();
+            } else if(tecla==="guardar" && juego_comienza==1){
+                guardar_partida();
+            } else if (tecla==="cargar"){
+                cargar_partida();
             } else if(juego_comienza==1){
                 colocar_letra(tecla);//---------------------OK
             } else{
-                alert("presiona jugar para iniciar!");
+                alert("Presiona Jugar para iniciar!");
             }
         } else if(tecla==="jugar_nuevamente"){
             reiniciar_pagina();
         } else{
-            //abrir modal correspondiente
-            console.log('entre el else if')
             abrir_modal();
         }
 
@@ -93,7 +103,7 @@ window.onload=function(){
         window.location.reload();
     }
 
-    //rellena letra en el casillero correspondiente--------------------------------OK
+    //rellena letra en la celda correspondiente--------------------------------OK
     function colocar_letra(tecla){
         if(letra<6){
             palabra[fila-1].querySelectorAll('.letra')[letra-1].innerText=tecla;
@@ -101,7 +111,7 @@ window.onload=function(){
         }
     }
 
-    //funcion borrar ultima letra ingresada--------------------------------------OK
+    //borrar ultima letra que se ingreso--------------------------------------OK
     function eliminar_letra(){
         var letras=palabra[fila-1].querySelectorAll('.letra');
         for (let index = letras.length-1; index >= 0; index--) {
@@ -117,12 +127,11 @@ window.onload=function(){
     //envia la palabra para compararla con today_word--------------------------OK
     function enviar_palabra(){
         if(letra<6){
-            alert("complete las 5 letras antes de enviar!");
+            alert("Completa las 5 letras para enviar la palabra!");
         } else{
             comparar_palabra();
             fila=fila+1;
             letra=1;
-            console.log('nro fila:'+fila)
         }
     }
 
@@ -154,12 +163,15 @@ window.onload=function(){
 
         if(letras_ok===5){
             detener_temp();
+            obtener_tiempo();
             palabra_encontrada=true;
             play_end=true;
             abrir_modal();
+            console.log("el tiempo fue de "+captura_temporizador[0]+captura_temporizador[1]+captura_temporizador[2]);
 
         } else if(fila===5){
             detener_temp();
+            obtener_tiempo();
             play_end=true;
             abrir_modal();
         }
@@ -176,6 +188,99 @@ window.onload=function(){
         modal.classList.remove('modal_off');
         modal.classList.add('modal_on');
         modal_abierto=true;
+    }
+
+    //obtiene datos temporizador
+    function obtener_tiempo(){
+        var min=document.getElementById("minutos");
+        var seg=document.getElementById("segundos");
+        var miles=document.getElementById("milesimas")
+
+        captura_temporizador=[min.innerText,seg.innerText,miles.innerText];
+        console.log("captura del tiempo: "+captura_temporizador[0]+captura_temporizador[1]+captura_temporizador[2]);
+
+    }
+
+    //carga los datos obtenidos del temporizador
+    function cargar_tiempo(){
+        document.getElementById("minutos").innerHTML=captura_temporizador[0];
+        document.getElementById("segundos").innerHTML=captura_temporizador[1];
+        document.getElementById("milesimas").innerHTML=captura_temporizador[2];
+    }
+
+    //obtiene celdas del tablero
+    function obtener_celdas(){
+        for (let fil = 0; fil < 5; fil++) {
+            for (let col = 0; col < 5; col++){
+                var celda=document.getElementById("f"+fil+"c"+col);
+                tablero_celdas.push(celda.innerText);
+            } 
+            
+        }
+    }
+
+    //cargar celdas de tablero_celdas en el tablero
+    function cargar_celdas(arreglo){
+        var p=0;
+        for (let f = 0; f < 6; f++) {
+            for (let col = 0; col < 5; col++) {
+                if(arreglo[p]==""){
+                    fila=f+1;
+                    letra=col+1;
+                    col=5;
+                    f=6
+                }else {
+                    document.getElementById("f"+f+"c"+col).innerHTML=arreglo[p];
+                    p=p+1;
+                    if(col==4){
+                        letra=6;
+                        enviar_palabra();
+                    }
+                }
+            }
+            
+        }
+    }
+
+    //guardar parida
+    function guardar_partida(){
+        detener_temp();
+        obtener_tiempo();
+        obtener_celdas();
+        var partidas_guardadas_f=window.localStorage.getItem("partidas_guardadas");
+        if(partidas_guardadas_f==null){
+            var partidas_guardadas=1;
+            window.localStorage.setItem("partidas_guardadas",partidas_guardadas);
+
+        }else{
+            partidas_guardadas=Number.parseInt(partidas_guardadas_f);
+            partidas_guardadas=partidas_guardadas+1;
+            window.localStorage.setItem("partidas_guardadas",partidas_guardadas);
+        }
+        var partida_objeto=new Object();
+        partida_objeto.nombreJugador=nombre_jugador.innerText;
+        partida_objeto.todayWord=today_word;
+        partida_objeto.numeroPartida=partidas_guardadas;
+        partida_objeto.tiempoActual=captura_temporizador;
+        partida_objeto.tableroCeldas=tablero_celdas;
+        window.localStorage.setItem("partida_actual",JSON.stringify(partida_objeto));
+        partidas_guardadas=partidas_guardadas+1;
+        alert("La partida se guardo con exito");
+        window.location.reload();
+    }
+
+
+    //cargar partida
+
+    function cargar_partida(){
+        var partida_recuperada=JSON.parse(localStorage.getItem("partida_actual"));
+        jugador=partida_recuperada.nombreJugador;
+        today_word=partida_recuperada.todayWord;
+        captura_temporizador=partida_recuperada.tiempoActual;
+        cargar_tiempo();
+        tablero_celdas=partida_recuperada.tableroCeldas;
+        cargar_celdas(tablero_celdas);
+        play_load=true;
     }
 
     //MODAL---------------------------------------------------OK
@@ -250,17 +355,6 @@ window.onload=function(){
         }
     }
 
-
-
-    /*
-    //para cerrar modal nombre click en pantalla
-    window.onclick = function(event) {
-        if (event.target == modal_nombre) {
-            modal_nombre.classList.remove('modal_on');
-            modal_nombre.classList.add('modal_off');
-        }
-    }
-    */
 
 }
 
